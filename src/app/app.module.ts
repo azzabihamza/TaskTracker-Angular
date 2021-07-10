0d5676e2fd1515799d17d69f4057ffc0ea162478
azzabihamza <hamza.azzabi@esprit.tn>
2021-07-10 12:59:10 +0100

    Add task to a json server and build a form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AddTaskComponent } from './components/add-task/add-task.component'
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task'
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css']
+})
+export class AddTaskComponent implements OnInit {
+  text!: string;
+  day!: string;
+  reminder: boolean= false;
+
+  @Output() onAddTask = new EventEmitter<Task>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(){
+    if (!this.text || !this.day){
+      alert('Please check your fields !');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+
+    this.onAddTask.emit(newTask);
+
+  }
+
+}
